4756a3697803ffb499ae953633ff040f4a3ad181
ship <543999860@qq.com>
2019-05-16 22:25:00 +0800

    up

--- definitions/nginx/virtualbox.sh
+++ definitions/nginx/virtualbox.sh
@@ -1,9 +1,9 @@
 # Installing the virtualbox guest additions
 
-VBOX_VERSION=$(cat /home/root/.vbox_version)
+VBOX_VERSION=$(cat /root/.vbox_version)
 cd /tmp
-mount -o loop /home/root/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+mount -o loop /root/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm -rf /home/root/VBoxGuestAdditions_*.iso
+rm -rf /root/VBoxGuestAdditions_*.iso
 
