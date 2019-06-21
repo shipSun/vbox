852b9c0a8ced5e14a6418038347a353170ac818b
ship <543999860@qq.com>
2019-06-21 13:11:53 +0800

    php5.6.40

---
+++ definitions/php5.6.40/base.sh
@@ -0,0 +1,20 @@
+# Base install
+
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+cat > /etc/yum.repos.d/epel.repo << EOM
+[epel]
+name=epel
+baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
+enabled=1
+gpgcheck=0
+EOM
+
+yum -y install gcc gcc-c++ make kernel kernel-devel kernel-headers zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms man acpid vim-common vim-enhanced ntp cmake
+
+# Make ssh faster by not waiting on DNS
+echo "UseDNS no" >> /etc/ssh/sshd_config
+
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+
+chkconfig --level 235 iptables off
---
+++ definitions/php5.6.40/virtualbox.sh
@@ -0,0 +1,11 @@
+# Installing the virtualbox guest additions
+
+yum -y install gcc*
+
+VBOX_VERSION=$(cat /home/veewee/.vbox_version)
+cd /tmp
+mount -o loop /home/veewee/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -rf /home/veewee/VBoxGuestAdditions_*.iso
+
